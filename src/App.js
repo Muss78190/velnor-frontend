e8b7491fd552d0fa54579471a76f9fb345577ea5
Muss78190 <benlahcen.mustapha@outlook.fr>
2025-05-21 23:21:35 +0200

    🧹 Nettoyage des pages inutiles et clarification des routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import Paiement48h from "./pages/Paiement48h";
-import Paiement24h from "./pages/Paiement24h";
-import AdminLogin from "./pages/AdminLogin";
+import LandingPageAudit from "./pages/LandingPageAudit";
+import Paiement24h from "./pages/paiement-24h";
+import Paiement48h from "./pages/paiement48h";
+import SuccessPage from "./pages/success";
+import CancelPage from "./pages/cancel";
 import AdminPayments from "./pages/AdminPayments";
-import SuccessPage from "./pages/SuccessPage";
 import MentionsLegales from "./pages/MentionsLegales";
-import Success from "./pages/success";
-import Cancel from "./pages/cancel";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/success" element={<Success />} />
-        <Route path="/cancel" element={<Cancel />} />   
         <Route path="/" element={<LandingPage />} />
-        <Route path="/paiement-48h" element={<Paiement48h />} />
+        <Route path="/audit" element={<LandingPageAudit />} />
         <Route path="/paiement-24h" element={<Paiement24h />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin" element={<AdminPayments />} />
+        <Route path="/paiement-48h" element={<Paiement48h />} />
         <Route path="/success" element={<SuccessPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
+        <Route path="/admin" element={<AdminPayments />} />
         <Route path="/mentions-legales" element={<MentionsLegales />} />
       </Routes>
     </Router>
